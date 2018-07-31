b8c678246cf9fc775ad1c20a7683e7f80e89c478
MrCuihi <649249762@qq.com>
2018-07-31 08:03:43 +0800

    小学期密码学实践
    images、js

---
+++ Threebears/Threebears/templates/js/jquery.wordsplit.js
@@ -0,0 +1,7 @@
+﻿//打开字滑入效果
+//Login.html,Register.html.Welcome.html,WelcomeHa.html
+//"Link the world. Share to world"滑动效果
+window.onload = function(){
+	$(".connect p").eq(0).animate({"left":"0%"}, 600);
+	$(".connect p").eq(1).animate({"left":"0%"}, 400);
+};
